f06590b5f1e26bcca60a3287ab582885318e9bdd
David M. Smiley <davidmsmiley@gmail.com>
2021-10-29 10:10:03 -0400

    Update fifa22app.py

--- fifa22app.py
+++ fifa22app.py
@@ -8,8 +8,8 @@ df = pd.read_csv(data_file, index_col='FullName')
 
 #User Selection _____________________________
 
-player_1 = st.selectbox('Select First Player: ', options=list(df.index.values))
-player_2 = st.selectbox('Select Second Player: ', options=list(df.index.values),
+player_1 = st.selectbox('Type in First Player: ', options=list(df.index.values))
+player_2 = st.selectbox('Type in Second Player: ', options=list(df.index.values),
                         index=1)
                         
 #Player Display_________________________________________
@@ -107,7 +107,7 @@ st.plotly_chart(fig)
 
 #Sidebar
 
-st.sidebar.markdown("### Need Help Finding a Player?")
+st.sidebar.markdown("### Need Help Selecting a Player?")
 country_club = st.sidebar.selectbox('What Country is Their Club In?', list(df['Club Country'].unique()))
 league = st.sidebar.selectbox('What League is Their Club in?', list(df['Club League'][df['Club Country'] == country_club].unique()))
 club = st.sidebar.selectbox('What Club Do They Play For?', list(df['Club'][df['Club League'] == league].unique()))
